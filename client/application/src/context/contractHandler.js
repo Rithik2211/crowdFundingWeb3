2f60bb313b686fc15b3fedbdb7175bbd6f7bfc46
Rithik2211 <rithikpranao22@gmail.com>
2024-01-25 21:41:16 +0530

    updated code

--- client/application/src/context/contractHandler.js
+++ client/application/src/context/contractHandler.js
@@ -1,13 +1,13 @@
-import {ethers, BrowserProvider, Contract} from 'ethers';
+import {ethers, Web3Provider, Contract} from 'ethers';
 import abi from './abi.json';
 
 const contractAddress = '0x4E64af83F6F6C6163c70Ecd1DbA91c87B1C7Df2a';
 export const updateEthers = async() => {
     try{
-        let provider = new BrowserProvider(window.ethereum);
+        let provider = new Web3Provider(window.ethereum);
         let signer = await provider.getSigner();
         let contractSigner = new Contract(contractAddress, abi, signer);
-        return await contractSigner.handleGetDonators(0);
+        let contract = await contractSigner.handleGetDonators(0);
     }
     catch(error){
         console.log("updatedEthers",error);
--- client/application/src/pages/Payments.js
+++ client/application/src/pages/Payments.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { updateEthers } from '../context/contractHandler';
+// import { updateEthers } from '../context/contractHandler';
 
 const Payments = () => {
     const [data, setData] = useState(null);
     useEffect(()=>{
         async function fetch(){
-        const contract = await updateEthers();
+        // const contract = await updateEthers();
         // let tempData = await contract.handleGetDonators();
         // setData(tempData);
-        console.log("@contract",contract);
+        console.log("@contract");
         }
         fetch();
     },[]);
