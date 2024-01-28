e2f6de170e9bb7ddfe317af44676a165f6975c41
Rithik2211 <rithikpranao22@gmail.com>
2024-01-28 21:42:07 +0530

    hard config

--- crowdfundingweb3/hardhat.config.js
+++ crowdfundingweb3/hardhat.config.js
@@ -1,22 +1,6 @@
-require("@matterlabs/hardhat-zksync-solc");
+require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.17",
-    defaultNetwork : 'sepolia',
-    networks: {
-      hardhat: {},
-      sepolia : {
-        url : 'https://rpc.ankr.com/eth_sepolia',
-        accounts : [`0x${process.env.PRIVATE_KEY}`]
-      }
-    },
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      }
-    }
-  },
+  solidity: "0.8.19",
 };
