75b55b07189afb5942bde67dc8316d8a4d489f5a
Rithik2211 <rithikpranao22@gmail.com>
2024-01-31 23:22:44 +0530

    updated abi file

--- crowdfundingweb3/test/Lock.js
+++ crowdfundingweb3/test/Lock.js
@@ -7,7 +7,7 @@ describe("CrowdFunding", function () {
 
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
+    const Lock = await ethers.getContractFactory("CrowdFunding");
     const lock = await Lock.deploy();
 
     return { lock, owner, otherAccount };
@@ -24,25 +24,6 @@ describe("CrowdFunding", function () {
       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
 
       expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
     });
   });
 
